686469a62ded92bf626e250b41932c044116224e
Your Name <aashardcustomz@gmail.com>
2025-09-11 20:59:02 +0300

    vvvv

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -160,11 +160,21 @@ export default function CreateAccount() {
     // Update border styles
     const updateBorders = () => {
       const borders = getBorderStyles(step);
+      const containerRef = useRef<HTMLDivElement>(null);
+      
       if (containerRef.current) {
-        const topBorder = containerRef.current.querySelector('.border-top');
-        const rightBorder = containerRef.current.querySelector('.border-right');
-        const bottomBorder = containerRef.current.querySelector('.border-bottom');
-        const leftBorder = containerRef.current.querySelector('.border-left');
+        const borders = {
+          top: { width: 0, height: 2 },
+          right: { width: 2, height: 0 },
+          bottom: { width: 0, height: 2 },
+          left: { width: 2, height: 0 }
+        };
+    
+        const topBorder = containerRef.current.querySelector('.border-top') as HTMLElement;
+        const rightBorder = containerRef.current.querySelector('.border-right') as HTMLElement;
+        const bottomBorder = containerRef.current.querySelector('.border-bottom') as HTMLElement;
+        const leftBorder = containerRef.current.querySelector('.border-left') as HTMLElement;
+    
         if (topBorder) topBorder.style.width = `${borders.top.width}px`;
         if (rightBorder) rightBorder.style.height = `${borders.right.height}px`;
         if (bottomBorder) bottomBorder.style.width = `${borders.bottom.width}px`;
